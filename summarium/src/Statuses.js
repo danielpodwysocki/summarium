4b1acdc7969f51ab31e095af888fa07fcf1e1072
Daniel Podwysocki <daniel@dapo.tech>
2022-03-23 23:35:56 +0100

    feat: create basic structure of the page

---
+++ summarium/src/Statuses.js
@@ -0,0 +1,18 @@
+function Status(data){
+    return(
+        <div class="Status">
+            placeholder
+        </div>
+    );
+}
+
+function Statuses(){
+    return(
+        <div class="Statuses">
+            <Status data="test"></Status>
+        </div>
+    );
+}
+
+
+export default Statuses;
