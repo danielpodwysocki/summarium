3bea4ca70aa68799dbea095da3f9c16cdb61cff9
Daniel Podwysocki <daniel@dapo.tech>
2022-03-27 13:02:10 +0200

    feat: render bullet-point data about our status

--- summarium/src/Statuses.js
+++ summarium/src/Statuses.js
@@ -1,15 +1,23 @@
-function Status(data){
+function Status(props){
     return(
         <div className="Status">
-            placeholder
+            <h1>{props.title}</h1>
+            <ul>
+            {props.bullet_points.map(
+                kv_pair => 
+                <li key={kv_pair['key']}> {kv_pair['key']}: {kv_pair['value']} </li>)
+            }
+
+            </ul>
         </div>
     );
 }
 
 function Statuses(){
+    let bullet_points = [{'key': 'ping', 'value':'100ms'}, {'key':'happy_days', 'value': 'True'}]
     return(
         <div className="Statuses">
-            <Status data="test"></Status>
+            <Status title="my test status" bullet_points={bullet_points}></Status>
         </div>
     );
 }
