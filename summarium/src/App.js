dd376b081f485ed93192441ff4913b74a1ed6234
Daniel Podwysocki <daniel@dapo.tech>
2022-04-09 15:41:09 +0200

    feat: pass the statuses from App to Statuses component

--- summarium/src/App.js
+++ summarium/src/App.js
@@ -3,10 +3,27 @@ import Header from './Header.js';
 import Statuses from './Statuses';
 
 function App() {
+  /*
+  Each status is an object with the following fields:
+    - title str() - the title of the status
+    - bullet_points - an array containing dicts with the following keys:
+        - key str()- name of the bullet point (for example "latency")
+        - value str() - value to be displayed (for example 100ms)
+  */
+  let statuses = [
+    {
+      "title": "My test status",
+      "bullet_points": [{'key': 'ping', 'value':'100ms'}, {'key':'happy_days', 'value': 'True'}]
+    },
+    {
+      "title": "My other status",
+      "bullet_points": [{'key': 'ping', 'value':'200ms'}, {'key':'happy_days', 'value': 'Of course!'}]
+    }
+  ];
   return (
     <div className="App">
       <Header></Header>
-      <Statuses></Statuses>
+      <Statuses statuses={statuses}></Statuses>
     </div>
   );
 }
--- summarium/src/Statuses.js
+++ summarium/src/Statuses.js
@@ -1,11 +1,16 @@
 function Status(props){
+    console.log("rendering status: "  + props.title)
+    console.log("props:")
+    console.log(props)
     return(
         <div className="Status">
             <h1>{props.title}</h1>
             <ul>
-            {props.bullet_points.map(
-                kv_pair => 
-                <li key={kv_pair['key']}> {kv_pair['key']}: {kv_pair['value']} </li>)
+            {
+                props.bullet_points.map(
+                    kv_pair => 
+                   <li key={kv_pair['key']}> {kv_pair['key']}: {kv_pair['value']} </li>
+                )
             }
 
             </ul>
@@ -13,11 +18,18 @@ function Status(props){
     );
 }
 
-function Statuses(){
+function Statuses(props){
+    let statuses = []
     let bullet_points = [{'key': 'ping', 'value':'100ms'}, {'key':'happy_days', 'value': 'True'}]
+
+    props.statuses.forEach(status => {
+        console.log(status.title)
+        statuses.push(<Status title={status.title} bullet_points={bullet_points}></Status>)
+    });
+
     return(
         <div className="Statuses">
-            <Status title="my test status" bullet_points={bullet_points}></Status>
+            {statuses}
         </div>
     );
 }
